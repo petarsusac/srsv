5c2af919243d03b9643c97a674b1f9b5ec8a7e91
Petar Susac <petar.susac@fer.hr>
2022-10-29 19:14:39 +0200

    Dodaj funkcije za odgodu u time_utils

--- lab1/inc/time_utils.h
+++ lab1/inc/time_utils.h
@@ -1,3 +1,5 @@
 void time_utils_init();
 void time_utils_print_timestamp(const char *msg);
 unsigned long time_utils_get_time_ms();
+void time_utils_delay_until(unsigned long ms);
+void time_utils_delay_for(unsigned long ms);
