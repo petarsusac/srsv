207fa665d32e6a63b9a255dcd14661e645522967
Petar Susac <petar.susac@fer.hr>
2023-01-05 21:10:16 +0100

    Napravljena simulacija x ms

--- lab3/inc/time_utils.h
+++ lab3/inc/time_utils.h
@@ -6,5 +6,6 @@ void time_utils_print_timestamp(const char *msg);
 unsigned long time_utils_get_time_ms();
 void time_utils_delay_until(unsigned long ms);
 void time_utils_delay_for(unsigned long ms);
+void time_utils_simulate_ms(int ms);
 
 #endif
--- lab3/time_utils.c
+++ lab3/time_utils.c
@@ -3,11 +3,14 @@
 #include <time.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define CLOCK CLOCK_MONOTONIC
 
 static pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 static struct timespec start_time;
+
+static unsigned long num_iterations_10_ms_delay;
 
 static void timespec_add(struct timespec *A, struct timespec *B)
 {
@@ -41,9 +44,52 @@ static void ms_to_timespec(unsigned long ms, struct timespec *t)
     t->tv_nsec = (ms % 1000) * 1000000;
 }
 
+static void run_for_10_ms()
+{
+    for (unsigned long i = 0; i < num_iterations_10_ms_delay; i++)
+    {
+        asm volatile("" ::: "memory");
+    }
+}
+
+static void calc_num_iterations_10_ms_delay()
+{
+    num_iterations_10_ms_delay = 1000000;
+    bool run = true;
+    unsigned long t0;
+    unsigned long t1;
+
+    while (run)
+    {
+        t0 = time_utils_get_time_ms();
+        run_for_10_ms();
+        t1 = time_utils_get_time_ms();
+        if (t1 - t0 >= 10)
+        {
+            run = false;
+        }
+        else
+        {
+            num_iterations_10_ms_delay *= 10;
+        }
+
+    }
+    
+    num_iterations_10_ms_delay = num_iterations_10_ms_delay * (10. / (t1 - t0));
+}
+
 void time_utils_init()
 {
     clock_gettime(CLOCK, &start_time);
+    calc_num_iterations_10_ms_delay();
+}
+
+void time_utils_simulate_ms(int ms)
+{
+    for (unsigned long i = 0; i < ms / 10; i++)
+    {
+        run_for_10_ms();
+    }
 }
 
 void time_utils_print_timestamp(const char *msg)
